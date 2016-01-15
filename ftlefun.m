45bb4acb15b8a65c39bae07e70381e53e968632f
asteyer <a518s941@ku.edu>
2016-01-15 12:12:48 -0600

    Create ftlefun.m
    This function returns an approximation of (1/t)*\int_{0}^{t} B_{i,i}(s)ds .

---
+++ ftlefun.m
@@ -0,0 +1,48 @@
+% getQdot returns the matrix right hand side of the equation dQ/dt=Q*S
+%
+% Inputs: T, X, Ttransient, prob, work, d , p
+% T - vector of times
+% X - vector of solutions of u'=f(t,u) and Q'=S*Q
+% prob - A scalar number that corresponds to the ODE being solved (used to change between different
+% problems
+% work - A vector of parameters of arbitrary size
+% d - The dimension of the problem
+% p - the number of LEs being approximated
+%
+% Outputs: appules, applles
+% appules - p x 1 vector of the approximate upper Lyapunov exponents
+% applles - p x 1 vector of the apporximate lower Lypaunov exponents
+function [Tftle,ftle] = ftlefun(T,X,prob,work,d,p)
+    Tn=length(T); 
+    u=zeros(d,1);
+    Bint=zeros(p,1); 
+    [t_delta, t_ind] = min(abs(T-.1));   
+    for n=t_ind:Tn-1
+        t=T(n); h=T(n+1)-T(n);
+        u(1:d)=X(n,1:d);
+        up=frhs(t,u,d,prob,work);
+        A=getA(t,u,up,d,prob,work) ;
+        Q=reshape(X(n,d+1:d+d*p),d,p);
+        QtAQ=Q'*A*Q;
+        S=zeros(p,p);
+        for i = 1:p
+            for j = 1:p
+                if i > j
+                    S(i,j) = QtAQ(i,j);
+                elseif i < j
+                    S(i,j) = -QtAQ(j,i);
+                else % i = j
+                    S(i,j) = 0;
+                end
+            end       
+        end
+        B=triu(QtAQ-S);
+        for j=1:p
+            Bint(j)=Bint(j)+h*B(j,j);
+        end  
+        Tftle(n-t_ind+1)=t;
+        for j=1:p
+            ftle(n-t_ind+1,j)=Bint(j)/t;
+        end
+    end       
+end
