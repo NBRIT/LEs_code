f220628aaff356a9cfd4902e74bce3ee12d59080
asteyer <a518s941@ku.edu>
2016-01-15 10:35:38 -0600

    Update frhs.m
    Combined frhs.m to contain all the examples in the paper

--- frhs.m
+++ frhs.m
@@ -27,13 +27,43 @@ function f=frhs(t,u,d,prob,work)
         f(1)=sigma*(u(2)-u(1));
         f(2)=u(1)*(rho-u(3))-u(2);
         f(3)=u(1)*u(2)-beta*u(3);
-    elseif prob==2
+    elseif prob==2 %% this is the Bistable Logistic Code
         del=work(1);
          b0=1e-6;
          b1=(1-b0)/b0;
          a=work(2);
          b=exp(a*t)/(b1+exp(a*t));      
          f(1)=-(u(1)-b)*(u(1)-b-del)*(u(1)-b+work(3)*del);
+    elseif prob ==3 %% this is the Unique Logistic Code
+        del=work(1);
+        b0=1e-6;
+        b1=(1-b0)/b0;
+        a=work(2);
+        b=exp(a*t)/(b1+exp(a*t));
+        f(1)=-(u(1)-b)*(u(1)-b-del);
+    elseif prob ==4 % Bistable Linear code
+        del=work(1);
+        a=work(2);
+        b0=0;
+        b=a*t+b0;
+        f(1)=-(u(1)-b)*(u(1)-b-del)*(u(1)-b+del);
+    elseif prob ==5 %% this is the Unique Linear Code
+        del=work(1);
+        a=work(2);
+        b0=0;
+        b=a*t+b0;
+        f(1)=-(u(1)-b)*(u(1)-b-del);
+    elseif prob==6
+	a=work(2);
+        b0=0;
+        b=a*t+b0;
+        epsilon=work(1);
+        CapitalPi=1.055;
+        r=0.01;
+        alpha=log(2.5)/10;
+        lambda=5.049e6;
+        BigA=3.9e7;
+        f(1)= (1/epsilon)*(u(2)*r*exp(alpha*u(1))-(lambda/BigA)*(u(1)-b));
+        f(2)=CapitalPi-u(2)*r*exp(alpha*u(1));
     end
- 
 end
