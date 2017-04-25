b8445d7c115b86f2df4b46401cc926ce0dcf1054
asteyer <a518s941@ku.edu>
2017-04-25 17:36:46 -0600

    Update rk_step.m

--- rk_step.m
+++ rk_step.m
@@ -1,4 +1,4 @@
-function xnew= rk_step(t,x,d,prob,h,Ark,b,c,s,par)
+function [xnew1,xnew2]= rk_step(t,x,d,prob,h,Ark,b1,b2,c,s,par)
     g=zeros(s,d);
     for i=1:s
         if i ==1
@@ -7,5 +7,6 @@ function xnew= rk_step(t,x,d,prob,h,Ark,b,c,s,par)
             g(i,:)=frhs(t+c(i)*h,x+h*Ark(i,1:i-1)*g(1:i-1,1),d,prob,par)'; 
         end
     end
-    xnew=x+h*g(:,:)'*b;
+    xnew1=x+h*g(:,:)'*b1;
+    xnew2=x+h*g(:,:)'*b2;
 end
